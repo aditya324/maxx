93fe35ae77036f71289a2b8a88b9b6050180da4d
aditya324 <adityakulkarni54321@gmail.com>
2025-12-16 10:27:16 +0530

    changes

--- header.php
+++ header.php
@@ -191,7 +191,7 @@
       <ul class="hidden md:flex flex-1 justify-evenly text-[#6b2e26] font-normal text-lg ">
         <li><a href="#" class="">About Us</a></li>
         <li class="special">
-          <a href="#" class="hover:text-[#a6533f]" id="specialToggle">Our Specialities</a>
+          <a href="#" class="hover:text-[#a6533f] cursor-pointer" id="specialToggle">Our Specialities</a>
 
           <!-- MEGA (keeps your look but plain-css controlled visibility) -->
           <div class="mega" aria-hidden="true" id="megaMenu">
